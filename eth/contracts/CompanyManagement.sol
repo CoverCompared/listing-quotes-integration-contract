5c87801fb750b488517bd93cc11a5c8c8e8c9ee0
Daksh Joshi <64137744+dakshjoshi@users.noreply.github.com>
2021-07-29 16:12:30 +0530

    Update CompanyManagement.sol

--- eth/contracts/CompanyManagement.sol
+++ eth/contracts/CompanyManagement.sol
@@ -1,56 +1,61 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-contract CompanyManagement {
+import "../node_modules/@openzeppelin/contracts/access/AccessControlEnumerable.sol";
+
+contract CompanyManagement is AccessControlEnumerable {
+    bytes32 public constant UPDATE_ROLE = keccak256("UPDATE_ROLE");
+
+    /**
+     * @dev Grants `DEFAULT_ADMIN_ROLE`, `UPDATE_ROLE` to the
+     * account that deploys the contract.
+     */
+
     constructor() {
-        owner = payable(msg.sender);
+        owner = msg.sender;
+
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _setupRole(UPDATE_ROLE, _msgSender());
     }
 
-    address payable owner;
+    address owner;
 
-    modifier onlyOwnerCanDoIt {
-        require(msg.sender == owner, "You are not the owner!");
-        _;
+    enum status {
+        A,
+        B,
+        S,
+        N,
+        V
     }
-
-    enum status {A, B, S, N, V}
-    //ACTIVE, BANNED, SUSPENDED, NOTACTIVE, VERYSOON
     status choice;
 
     struct Company {
-        string name;
-        string link;
+        bytes32 name;
+        bytes32 link;
         status Status;
-        // mapping(uint256 => Product) products;
-        // uint256[] productIDX;
-        string apiProductListing;
-        string apiQuoteListing;
+        bytes32 apiProductListing;
+        bytes32 apiQuoteListing;
     }
 
-    //Things to add
-    //4. Adding an array of struct in the Company Struct regarding the policies they offer.
+    mapping(address => Company) companies;
+    address[] public addresses;
 
-    // struct Product {
-    //     uint256 productID;
-    //     string productName;
-    //     productStatus Status;
-    //     // uint256 productPrice;
-    //     // string currency;
-    //     // uint256 productCoverageDates;
-    //     // uint256 productPremium;
-    // }
-    // enum productStatus {ACTIVE, NOTACTIVE}
-
-    mapping(uint256 => Company) companies;
-    uint256[] public idx;
+    /**
+     * @dev Add company
+     * only accessible by user having `UPDATE_ROLE`
+     * or account that deploys the contract.
+     */
 
     function setCompany(
         status _status,
-        string memory link,
-        string memory name,
-        string memory apiProduct,
-        string memory apiQuote
-    ) public onlyOwnerCanDoIt {
-        Company storage company = companies[idx.length];
+        bytes32 link,
+        bytes32 name,
+        bytes32 apiProduct,
+        bytes32 apiQuote,
+        address _address
+    ) public {
+        require(hasRole(UPDATE_ROLE, _msgSender()));
+        Company storage company = companies[_address];
 
         company.name = name;
         company.link = link;
@@ -58,126 +63,31 @@ contract CompanyManagement {
         company.apiProductListing = apiProduct;
         company.apiQuoteListing = apiQuote;
 
-        idx.push(idx.length + 1);
+        addresses.push(_address);
     }
 
     function getIdxLength() public view returns (uint256) {
-        return idx.length;
+        return addresses.length;
     }
 
-    function getCompany(uint256 id)
+    function getCompany(address _address)
         public
         view
-        returns (
-           
-            string memory,
-            string memory,
-            status,
-            string memory,
-            string memory
-        )
+        returns (Company memory company)
     {
-        return (
-            companies[id].link,
-            companies[id].name,
-            companies[id].Status,
-            companies[id].apiProductListing,
-            companies[id].apiQuoteListing
-        );
+        return (companies[_address]);
     }
 
-    // function getProduct(uint256 _id, uint256 _productID)
-    //     public
-    //     view
-    //     returns (string memory _productName, productStatus _status)
-    // {
-    //     return (
-    //         companies[_id].products[_productID].productName,
-    //         companies[_id].products[_productID].Status
-    //     );
-    // }
+    /**
+     * @dev Updae status of company
+     * only accessible by user having `UPDATE_ROLE`
+     * or account that deploys the contract.
+     */
 
-    // function getProductListLength(uint256 _id)
-    //     public
-    //     view
-    //     returns (uint256 length)
-    // {
-    //     return (companies[_id].productIDX.length);
-    // }
-
-    // function updateProductStatus(
-    //     productStatus _status,
-    //     uint256 _companyId,
-    //     uint256 _productId
-    // )
-    //     public
-    //     returns (
-    //         uint256 _productID,
-    //         productStatus _newStatus,
-    //         string memory _productName
-    //     )
-    // {
-    //     Product storage product = companies[_companyId].products[_productId];
-
-    //     product.Status = _status;
-
-    //     return (product.productID, product.Status, product.productName);
-    // }
-
-    // function addProduct(
-    //     uint256 _id,
-    //     uint256 _productID,
-    //     productStatus _status,
-    //     string memory _productName
-    // ) public onlyOwnerCanDoIt {
-    //     Product storage product =
-    //         companies[_id].products[companies[_id].productIDX.length];
-
-    //     product.productID = _productID;
-    //     product.productName = _productName;
-    //     product.Status = _status;
-
-    //     companies[_id].productIDX.push(companies[_id].productIDX.length + 1);
-    // }
-    
-    
-    function updateStatus(status _status, uint256 _id)
-        public
-        onlyOwnerCanDoIt
-        returns (
-            string memory,
-            string memory,
-            status
-        )
-    {
-        Company storage company = companies[_id];
+    function updateStatus(status _status, address _address) public {
+        require(hasRole(UPDATE_ROLE, _msgSender()));
+        Company storage company = companies[_address];
         company.Status = _status;
-
-        return (
-            companies[_id].link,
-            companies[_id].name,
-            companies[_id].Status
-        );
-    }
-
-    function updateLogo(string memory logoLink, uint256 _id)
-        public
-        onlyOwnerCanDoIt
-        returns (
-            
-            string memory,
-            string memory,
-            status
-        )
-    {
-        Company storage company = companies[_id];
-        company.link = logoLink;
-
-        return (
-           
-            companies[_id].link,
-            companies[_id].name,
-            companies[_id].Status
-        );
     }
 }
+
